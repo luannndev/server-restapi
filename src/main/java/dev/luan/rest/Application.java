8f0ae3f6980e47700103208a66cb6fdf62f56c71
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-21 01:05:06 +0200

    Add MongoDB connection settings to Application.java
    Imports required libraries and initializes MongoDB connection with specified settings. This includes setting the connection string, uuid representation, and selecting the target database.

--- src/main/java/dev/luan/rest/Application.java
+++ src/main/java/dev/luan/rest/Application.java
@@ -1,8 +1,12 @@
 package dev.luan.rest;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import lombok.extern.java.Log;
+import org.bson.UuidRepresentation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,6 +19,14 @@ public class Application {
 
     public static void main(String[] args) {
         try {
+            ConnectionString connectionString = new ConnectionString("mongodb://user:password@localhost:27017/");
+            MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                    .applyConnectionString(connectionString)
+                    .uuidRepresentation(UuidRepresentation.STANDARD)
+                    .build();
+
+            mongoClient = MongoClients.create(mongoClientSettings);
+            mongoDatabase = mongoClient.getDatabase("restapi");
 
         } catch (Exception exception) {
             exception.printStackTrace();
